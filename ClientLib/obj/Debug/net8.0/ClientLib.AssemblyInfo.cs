ace77838ee0a62c2980f1c6c70198a6b892c10f9
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-03-02 19:47:57 +0100

    Added baselib

--- ClientLib/obj/Debug/net8.0/ClientLib.AssemblyInfo.cs
+++ ClientLib/obj/Debug/net8.0/ClientLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c3a5c2deaa9ec32a0704c67aa00962890ef21e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f660d42e631986f60f6421d6ca93b8548fd7d7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
