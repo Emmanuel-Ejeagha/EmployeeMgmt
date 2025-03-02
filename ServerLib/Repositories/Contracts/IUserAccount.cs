e02ca478090d2f4fd71f922a9412546bff293b0d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-03-02 19:56:00 +0100

    Contraccts and Implementation

---
+++ ServerLib/Repositories/Contracts/IUserAccount.cs
@@ -0,0 +1,12 @@
+using BaseLib.DTOs;
+using BaseLib.Responses;
+
+namespace ServerLib.Repositories.Contracts
+{
+    public interface IUserAccount
+    {
+        Task<GeneralResponse> CreateAsync(Register user);
+        Task<LoginResponse> SignInAsync(Login user);
+        Task<LoginResponse> RefreshTokenAsync(RefreshToken token);
+    }
+}
