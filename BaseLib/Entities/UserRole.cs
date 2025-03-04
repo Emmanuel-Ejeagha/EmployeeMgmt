1b253f23414ab0a7b74b3407901e0824dc1ab0f6
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-03-04 22:03:06 +0100

    Added user role

---
+++ BaseLib/Entities/UserRole.cs
@@ -0,0 +1,9 @@
+namespace BaseLib.Entities
+{
+    public class UserRole
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+    }
+}
