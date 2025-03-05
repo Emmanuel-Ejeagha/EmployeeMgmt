e062ef6878a51eb78b0291a142a1ef49c05d2ba2
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-03-05 22:39:01 +0100

    Added: DialogService

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,31 @@
+using Blazored.LocalStorage;
+using Client;
+using ClientLib.Helpers;
+using ClientLib.Services.Contracts;
+using ClientLib.Services.Implementation;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
+using Syncfusion.Blazor.Popups;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazoredLocalStorage();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<CustomHttpHandler>();
+builder.Services.AddHttpClient("SystemApiClient", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7096/");
+}).AddHttpMessageHandler<CustomHttpHandler>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7096/") });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<GetHttpClient>();
+builder.Services.AddScoped<LocalStorageService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<IUserAccountService, UserAccountService>();
+
+builder.Services.AddScoped<SfDialogService>();
 
 await builder.Build().RunAsync();
